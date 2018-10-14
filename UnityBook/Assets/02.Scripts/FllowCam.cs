2c9edb918f9b20a2701dcaf6e569c36bd17f903e
Elliot Hongyun Lee <hylee101001@gmail.com>
2018-10-13 20:22:53 -0400

    init

---
+++ UnityBook/Assets/02.Scripts/FllowCam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FllowCam : MonoBehaviour {
+    public Transform targetTr;
+    public float dist = 10.0f;
+    public float height = 3;
+    public float dampTrace = 20;
+    private Transform tr;
+	
+	void Start () {
+        tr = GetComponent<Transform>();
+	}
+
+    private void LateUpdate()
+    {
+        tr.position = Vector3.Lerp(tr.position, targetTr.position - (targetTr.forward * dist) + (Vector3.up * height), Time.deltaTime * dampTrace);
+        tr.LookAt(targetTr.position);
+    }
+}
---
+++ UnityBook/Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+    private float h = 0, v = 0;
+    private Transform tr;
+    public float moveSpeed = 10.0f;
+    public float rotSpeed = 100;
+    void Start () {
+        tr = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        
+        Debug.Log("H = " + h.ToString());
+        Debug.Log("V = " + v.ToString());
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
+        tr.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Mouse X"));
+    }
+}
