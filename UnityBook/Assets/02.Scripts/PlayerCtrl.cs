b1fb6ed80fbbfb9f67c0082ad256315473cc7413
Elliot Hongyun Lee <hylee101001@gmail.com>
2018-10-13 22:30:55 -0400

    aasdfasdf

--- UnityBook/Assets/02.Scripts/PlayerCtrl.cs
+++ UnityBook/Assets/02.Scripts/PlayerCtrl.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Anim
+{
+    public AnimationClip idle, runForward, runBackward, runRight, runLeft;
+}
+
 public class PlayerCtrl : MonoBehaviour {
     private float h = 0, v = 0;
     private Transform tr;
     public float moveSpeed = 10.0f;
     public float rotSpeed = 100;
+
+    public Anim anim;
+    public Animation _animation;
+
     void Start () {
         tr = GetComponent<Transform>();
+        _animation = GetComponentInChildren<Animation>();
+
+        _animation.clip = anim.idle;
+        _animation.Play();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
@@ -21,5 +34,11 @@ public class PlayerCtrl : MonoBehaviour {
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
         tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
         tr.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Mouse X"));
+
+
+        if (v >= 0.1f) _animation.CrossFade(anim.runForward.name, 0.3f);
+        if (v <= -0.1f) _animation.CrossFade(anim.runBackward.name, 0.3f);
+        if (h >= 0.1f) _animation.CrossFade(anim.runLeft.name, 0.3f);
+        if (h <= -0.1f) _animation.CrossFade(anim.runRight.name, 0.3f);
     }
 }
---
+++ UnityBook/Assets/02.Scripts/WallCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCtrl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "BULLET") Destroy(collision.gameObject);
+    }
+}
